ab6f90929de68b3b652a1fa1ff0b7510a9f0943b
Samat <Shaiykov7@mail.ru>
2022-05-11 21:36:31 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//учебная программа
+
+//Шайыков, Сатымбаев КЭ-418, 2022
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+
+    cout << "Enter X: "; cin>>x;
+    cout << "Enter Y: "; cin>>y;
+
+    cout << "X + Y = "<<x+y<<endl;
+    cout << "X - Y = "<<x-y<<endl;
+    cout << "X * Y = "<<x*y<<endl;
+    return 0;
+}
